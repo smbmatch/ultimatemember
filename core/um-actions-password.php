e9a285db6ad5f719c64fa968372a4daebea98d17
Denise Draper <draperd@acm.org>
2016-09-10 01:17:25 -0700

    Add debugging of "spam bot" messages and revert a change of theirs that would limit login names to 30 characters.

--- core/um-actions-password.php
+++ core/um-actions-password.php
@@ -87,8 +87,11 @@
 	function um_reset_password_errors_hook( $args ) {
 		global $ultimatemember;
 
-		if ( $_POST[ $ultimatemember->honeypot ] != '' )
+		if ( $_POST[ $ultimatemember->honeypot ] != '' ) {
+			smb_error("trying to figure out how spam bot response gets triggered");
+			smb_error($_POST);
 			wp_die('Hello, spam bot!');
+		}
 
 		$form_timestamp  = trim($_POST['timestamp']);
 		$live_timestamp  = current_time( 'timestamp' );
@@ -149,14 +152,20 @@
 	function um_change_password_errors_hook( $args ) {
 		global $ultimatemember;
 
-		if ( $_POST[ $ultimatemember->honeypot ] != '' )
+		if ( $_POST[ $ultimatemember->honeypot ] != '' ) {
+			smb_error("trying to figure out how spam bot response gets triggered");
+			smb_error($_POST);
 			wp_die('Hello, spam bot!');
+		}
 
 		$form_timestamp  = trim($_POST['timestamp']);
 		$live_timestamp  = current_time( 'timestamp' );
 
-		if ( $form_timestamp == '' && um_get_option('enable_timebot') == 1 )
-			wp_die( __('Hello, spam bot!') );
+		if ( $form_timestamp == '' && um_get_option('enable_timebot') == 1 ) {
+			smb_error("trying to figure out how spam bot response gets triggered");
+			smb_error($_POST);
+			wp_die(__('Hello, spam bot!'));
+		}
 
 		if ( $live_timestamp - $form_timestamp < 3 && um_get_option('enable_timebot') == 1 ){
 			wp_die( __('Whoa, slow down! You\'re seeing this message because you tried to submit a form too fast and we think you might be a spam bot. If you are a real human being please wait a few seconds before submitting the form. Thanks!') );
